735e385d7ab5bc5737bcf04f3a1f71aa2e8ba25f
Justin Turner <jmturner638@gmail.com>
2021-07-30 09:33:45 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,31 @@
 def main():
     #write your code below this line
+    sum_of_nums = 0
+    count = 0
+    evens = 0
+    odds = 0
+
+    while True:
+        number = int(input("Give numbers:"))
+
+        if number == -1:
+            break
+
+        sum_of_nums += number
+        count += 1
+
+        if number % 2 == 0:
+            evens += 1
+        else:
+            odds += 1
+
+    
+    print("Thx! Bye!")
+    print("Sum: " + str(sum_of_nums))
+    print("Numbers: " + str(count))
+    print("Average: " + str(sum_of_nums/count))
+    print("Even: " + str(evens))
+    print("Odd: " + str(odds))
 
 if __name__ == '__main__':
     main()
